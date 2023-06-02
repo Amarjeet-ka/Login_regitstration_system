42549683c445120e4a3a46220a97dcb6e5903b46
Amarjeet-ka <116912077+Amarjeet-ka@users.noreply.github.com>
2023-06-02 07:19:38 +0530

    ......

--- frontend/src/components/logs/Logs.js
+++ frontend/src/components/logs/Logs.js
@@ -7,88 +7,175 @@ import { FaEnvelope, FaLock, FaGoogle } from 'react-icons/fa';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+
 export default function Logs() {
   const [key, setKey] = useState('home');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch('/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await response.json();
+      console.log(data); // Handle the response from the backend
+
+      // Reset the form inputs
+      setEmail('');
+      setPassword('');
+    } catch (error) {
+      console.log('Error occurred while sending data:', error);
+    }
+  };
 
   return (
-   <Container>
-    <Row><Col />
-      <Col md={4}>
-      <div style={{ justifyContent: 'center', marginTop: '150px' }}>
-      <Tabs id="controlled-tab-example" activeKey={key} onSelect={(k) => setKey(k)} className="mb-3">
-        <Tab eventKey="register" title="Register">
-          <div style={{ marginTop: '30px' }}>
-            <InputGroup className="mb-3">
-              <InputGroup.Text>
-                <FaEnvelope />
-              </InputGroup.Text>
-              <FormControl type="email" placeholder="Email" />
-            </InputGroup>
-            <InputGroup className="mb-3">
-              <InputGroup.Text>
-                <FaLock />
-              </InputGroup.Text>
-              <FormControl type="password" placeholder="Password" />
-            </InputGroup>
-            <Button variant="primary">Login</Button>
-            <div style={{ textAlign: 'center', marginTop: '10px' }}>
-              <Button variant="light">
-                <FaGoogle /> Signup with Gmail
-              </Button>
-            </div>
+    <Container>
+      <Row>
+        <Col />
+        <Col md={4}>
+          <div style={{ justifyContent: 'center', marginTop: '150px' }}>
+            <Tabs
+              id="controlled-tab-example"
+              activeKey={key}
+              onSelect={(k) => setKey(k)}
+              className="mb-3"
+            >
+              <Tab eventKey="register" title="Register">
+                <div style={{ marginTop: '30px' }}>
+                  <InputGroup className="mb-3">
+                    <InputGroup.Text>
+                      <FaEnvelope />
+                    </InputGroup.Text>
+                    <FormControl
+                      type="email"
+                      placeholder="Email"
+                      value={email}
+                      onChange={handleEmailChange}
+                    />
+                  </InputGroup>
+                  <InputGroup className="mb-3">
+                    <InputGroup.Text>
+                      <FaLock />
+                    </InputGroup.Text>
+                    <FormControl
+                      type="password"
+                      placeholder="Password"
+                      value={password}
+                      onChange={handlePasswordChange}
+                    />
+                    
+                  </InputGroup>
+                  <InputGroup className="mb-3">
+                    <InputGroup.Text>
+                      <FaLock />
+                    </InputGroup.Text>
+                    <FormControl
+                      type="password"
+                      placeholder="Re-Enter Password"
+                      value={password}
+                      onChange={handlePasswordChange}
+                    />
+                    
+                  </InputGroup>
+                  <Button variant="primary" onClick={handleFormSubmit}>
+                    Login
+                  </Button>
+                  <div style={{ textAlign: 'center', marginTop: '10px' }}>
+                    <Button variant="light">
+                      <FaGoogle /> Signup with Gmail
+                    </Button>
+                  </div>
+                </div>
+              </Tab>
+              <Tab eventKey="home" title="Student">
+                <div style={{ marginTop: '30px', marginRight: '0px' }}>
+                  <InputGroup className="mb-3">
+                    <InputGroup.Text>
+                      <FaEnvelope />
+                    </InputGroup.Text>
+                    <FormControl
+                      type="email"
+                      placeholder="Email"
+                      value={email}
+                      onChange={handleEmailChange}
+                    />
+                  </InputGroup>
+                  <InputGroup className="mb-3">
+                    <InputGroup.Text>
+                      <FaLock />
+                    </InputGroup.Text>
+                    <FormControl
+                      type="password"
+                      placeholder="Password"
+                      value={password}
+                      onChange={handlePasswordChange}
+                    />
+                  </InputGroup>
+                  <Button variant="primary" href="/Home">
+                    Login
+                  </Button>
+                  <div style={{ textAlign: 'center', marginTop: '10px' }}>
+                    <Button variant="light">
+                      <FaGoogle /> Signin with Gmail
+                    </Button>
+                  </div>
+                </div>
+              </Tab>
+              <Tab eventKey="master" title="Master">
+                <div style={{ marginTop: '30px' }}>
+                  <InputGroup className="mb-3">
+                    <InputGroup.Text>
+                      <FaEnvelope />
+                    </InputGroup.Text>
+                    <FormControl
+                      type="email"
+                      placeholder="Email"
+                      value={email}
+                      onChange={handleEmailChange}
+                    />
+                  </InputGroup>
+                  <InputGroup className="mb-3">
+                    <InputGroup.Text>
+                      <FaLock />
+                    </InputGroup.Text>
+                    <FormControl
+                      type="password"
+                      placeholder="Password"
+                      value={password}
+                      onChange={handlePasswordChange}
+                    />
+                  </InputGroup>
+                  <Button variant="primary" onClick={handleFormSubmit}>
+                    Login
+                  </Button>
+                  <div style={{ marginTop: '10px', textAlign: 'center' }}>
+                    <Button variant="light">
+                      <FaGoogle /> Signin with Gmail
+                    </Button>
+                  </div>
+                </div>
+              </Tab>
+            </Tabs>
           </div>
-        </Tab>
-        <Tab eventKey="home" title="Student">
-          <div style={{ marginTop: '30px', marginRight: '0px' }}>
-            <InputGroup className="mb-3">
-              <InputGroup.Text>
-                <FaEnvelope />
-              </InputGroup.Text>
-              <FormControl type="email" placeholder="Email" />
-            </InputGroup>
-            <InputGroup className="mb-3">
-              <InputGroup.Text>
-                <FaLock />
-              </InputGroup.Text>
-              <FormControl type="password" placeholder="Password" />
-            </InputGroup>
-            <Button variant="primary" href="/Home">
-              Login
-            </Button>
-            <div style={{ textAlign: 'center', marginTop: '10px' }}>
-              <Button variant="light">
-                <FaGoogle /> Signin with Gmail
-              </Button>
-            </div>
-          </div>
-        </Tab>
-        <Tab eventKey="master" title="Master">
-          <div style={{ marginTop: '30px' }}>
-            <InputGroup className="mb-3">
-              <InputGroup.Text>
-                <FaEnvelope />
-              </InputGroup.Text>
-              <FormControl type="email" placeholder="Email" />
-            </InputGroup>
-            <InputGroup className="mb-3">
-              <InputGroup.Text>
-                <FaLock />
-              </InputGroup.Text>
-              <FormControl type="password" placeholder="Password" />
-            </InputGroup>
-            <Button variant="primary">Login</Button>
-            <div style={{  marginTop: '10px' ,textAlign:'center'}}>
-              <Button variant="light">
-                <FaGoogle /> Signin with Gmail
-              </Button>
-            </div>
-          </div>
-        </Tab>
-      </Tabs>
-    </div>
-      </Col>
-      <Col />
-    </Row>
-   </Container>
+        </Col>
+        <Col />
+      </Row>
+    </Container>
   );
 }
--- frontend/src/pages/Career.jsx
+++ frontend/src/pages/Career.jsx
@@ -1,7 +1,163 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import Spinner from 'react-bootstrap/Spinner';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
-export default function Career() {
+const Career = () => {
+  const [data, setData] = useState(null);
+  const [filter, setFilter] = useState('');
+  const [filteredData, setFilteredData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const url =
+          'https://script.googleusercontent.com/macros/echo?user_content_key=K1SfY4XlD6qbGNRDMsv24cz3mKDQE1JnlXba9UCLupRXwydM0Op1uqTy30xeNrvvUZ3tB1EBxaMTecfjQO7WQuSb38Zru9Tpm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnFi1RJg8_gr138n49tlr-w5BxUJAIqK7W6X7NVx3ShgSAsA1v_pBUqYNU_wxkU3NtlMVmRyvLlNSswX2mvP2NJZI5FO2_jdUbA&lib=M2imxiyfpXxrrry3zOH5XEyXcxPGDFq9z';
+
+        const response = await fetch(url);
+
+        if (!response.ok) {
+          throw new Error('Request failed');
+        }
+
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (data && filter) {
+      const filtered = data.data.filter(
+        (job) =>
+          job.company_name.toLowerCase().includes(filter.toLowerCase()) ||
+          job.location.toLowerCase().includes(filter.toLowerCase())
+      );
+      setFilteredData(filtered);
+    } else {
+      setFilteredData(null);
+    }
+  }, [data, filter]);
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
   return (
-    <div>Career</div>
-  )
-}
+    <>
+      <div>
+      <div style={{ marginTop:'10%',marginLeft:'20%',display: 'flex', alignItems: 'center' }}>
+  <label htmlFor="filter" style={{ marginRight: '10px' }}>Filter:</label>
+  <input type="text" id="filter" name="filter" value={filter} onChange={handleFilterChange} style={{ padding: '5px' }} />
+</div>
+
+        {data ? (
+          <div className="job-list">
+            {filteredData && filteredData.length > 0 ? (
+              filteredData.map((job) => (
+                <Container key={job.job_id}>
+                  <Row>
+                    <Col md={3} />
+                    <Col md={6}>
+                      <div className="job-card">
+                        <h2 className="job-title">{job.title}</h2>
+                        <p className="job-company">{job.company_name}</p>
+                        <p className="job-location">{job.location}</p>
+                        <p className="job-description">{job.description}</p>
+                        <button className="job-apply-btn" style={{ color: 'white' }}>
+                          <a href={job.posted_at} target="_blank" rel="noopener noreferrer">
+                            Apply Now
+                          </a>
+                        </button>
+                        <p className="job-salary">{job.salary}</p>
+                      </div>
+                    </Col>
+                    <Col md={2} />
+                  </Row>
+                </Container>
+              ))
+            ) : (
+              <p   >No jobs found for the provided filter.</p>
+            )}
+          </div>
+        ) : (
+          <div style={{ textAlign: 'center', marginTop: '20%' }}>
+            <Spinner animation="border" size="44px" variant="primary" />
+            <Spinner animation="border" variant="secondary" />
+            <Spinner animation="border" variant="primary" />
+          </div>
+        )}
+
+        <style>
+          {`
+            .job-list {
+              display: grid;
+              grid-gap: 20px;
+            }
+
+            .job-card {
+              border: 1px solid #ccc;
+              padding: 10px;
+              background-color: #f3f3f3;
+            }
+
+            .job-title {
+              font-size: 18px;
+              font-weight: bold;
+            }
+
+            .job-company {
+              font-size: 14px;
+              color: black;
+            }
+
+            .job-location {
+              font-size: 14px;
+              margin-top: 5px;
+            }
+
+            .job-description {
+              margin-top: 10px;
+            }
+
+            .job-posted {
+              margin-top: 10px;
+              font-size: 12px;
+            }
+
+            .job-salary {
+              margin-top: 10px;
+              font-size: 14px;
+              font-weight: bold;
+            }
+
+            .job-apply-btn {
+              display: inline-block;
+              padding: 8px 16px;
+              background-color: #007bff;
+              color: black;
+              text-decoration: none;
+              font-size: 14px;
+              font-weight: bold;
+              border-radius: 4px;
+              border: none;
+              cursor: pointer;
+            }
+
+            .job-apply-btn:hover {
+              background-color: #0056b3;
+            }
+          `}
+        </style>
+      </div>
+    </>
+  );
+};
+
+export default Career;
